fc5f355e99b027f0a3e2faf236e365863b173073
simonKgithub <yonsei1994@gmail.com>
2022-08-20 15:17:51 +0900

    loginController 커밋

---
+++ src/main/java/grpstudy/grpware/login/controller/LoginController.java
@@ -0,0 +1,34 @@
+package grpstudy.grpware.login.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login/*")
+public class LoginController {
+    //접근 권한이 없는 경우
+    @GetMapping("/accessError.html")
+    public void accessError(Authentication auth, Model model){
+        model.addAttribute("msg", "접근 권한이 없습니다. 관리자에게 문의해주시기 바랍니다.");
+    }
+
+    //시큐리티 로그인 페이지 이동
+    @GetMapping("/login.html")
+    public void loginGET(String error, String logout, Model model){
+        if(error != null){
+            model.addAttribute("error", "아이디와 비밀번호를 확인해주시기 바랍니다.");
+        }
+        if(logout != null){
+            model.addAttribute("logout", "로그아웃되었습니다.");
+        }
+    }
+
+    //시큐리티 로그아웃 페이지 이동
+    @GetMapping("/logout.html")
+    public String logoutGET(){
+        return "login/login";
+    }
+}
