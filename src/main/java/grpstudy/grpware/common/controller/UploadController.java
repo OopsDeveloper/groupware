704f30c8ea21a9431983dae84a5d642d6614ca32
yonse <yonsei1994@gmail.com>
2023-03-04 16:47:37 +0900

    파일첨부관련 프로퍼티(경로 등) 재설정

--- src/main/java/grpstudy/grpware/common/controller/UploadController.java
+++ src/main/java/grpstudy/grpware/common/controller/UploadController.java
@@ -1,6 +1,7 @@
 package grpstudy.grpware.common.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,23 +18,13 @@ import java.util.Properties;
 @Slf4j
 @RequestMapping("/upload")
 public class UploadController {
+    //파일경로
+    @Value("${file.path}")
+    private String uploadPath;
 
     @PostMapping("/ajax.do")
     public void uploadAjax(MultipartFile[] uploadFile){
         log.info("File upload by ajax");
-        /*프로퍼티에서 경로 가져오는 방법
-        Properties prop = new Properties();
-        try{
-            prop.load(new FileInputStream("C:\\Users\\yonse\\IdeaProjects\\groupware\\src\\main\\webapp\\WEB-INF\\spring-config\\filepath.properties"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            log.info("파일의 경로를 찾을 수 없습니다.");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String uploadFolder = prop.getProperty("file.path");
-         */
-        String uploadPath = "C:\\upload\\temp";
 
         for (MultipartFile multipartFile : uploadFile) {
             String uploadFileName = multipartFile.getOriginalFilename();
